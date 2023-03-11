782cf62650218823bcca0d7406fc550bab8b0961
salah <salah.bouheniche@gmail.com>
2023-03-11 10:47:50 +0100

    read argv from the end so the value are stored in the right order

--- src/extract_values.c
+++ src/extract_values.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 13:39:24 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/03/06 22:49:34 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/03/11 10:44:02 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	extract_values(int_list **list, int argc, char **argv)
 	int i;
 	int	n;
 
-	i = 0;
-	while (++i < argc)
+	i = argc;
+	while (--i > 0)
 	{
 		n = ft_atoi(argv[i]);
 		push(list, n);
