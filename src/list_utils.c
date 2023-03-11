e0a71c403af0d1a3a3df1477cff2dfe49b1b327f
salah <salah.bouheniche@gmail.com>
2023-03-11 10:50:58 +0100

    rename push_to.c into list_utils.c

--- src/list_utils.c
+++ src/list_utils.c
@@ -1,25 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_to.c                                          :+:      :+:    :+:   */
+/*   push_to_a.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 21:47:26 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/03/06 21:53:37 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/03/11 00:20:55 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	push(int_list **p, int value)
+//Create a new element int the a_list given as argument
+void	push_to_a(int_list **list, int number)
 {
-	int_list *element;
+	int_list	*element;
 
 	element = malloc(sizeof(int_list));
 	if (!element)
 		return ;
-	element->number = value;
-	element->previous = *p;
-	*p = element;
+	element->number = number;
+	element->previous = *list;
+	*list = element;
+}
+
+void	clean_list(int_list **list)
+{
+	int_list *tmp;
+
+	if (*list)
+	{
+		tmp = (*list)->previous;
+		free(*list);
+		*list = tmp;
+	}
 }
