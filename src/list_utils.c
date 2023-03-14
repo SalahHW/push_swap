db5f8bcc5a19591b11f24a8ae0afa9713f332740
salah <salah.bouheniche@gmail.com>
2023-03-14 14:50:17 +0100

    linked lists are now doubly linked

--- src/list_utils.c
+++ src/list_utils.c
@@ -1,38 +1,66 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_to_a.c                                        :+:      :+:    :+:   */
+/*   list_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 21:47:26 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/03/11 00:20:55 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/03/14 07:51:38 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/push_swap.h"
+#include "../include/push_swap.h"
 
-//Create a new element int the a_list given as argument
-void	push_to_a(int_list **list, int number)
+void	init(edge *list)
 {
-	int_list	*element;
-
-	element = malloc(sizeof(int_list));
-	if (!element)
-		return ;
-	element->number = number;
-	element->previous = *list;
-	*list = element;
+	list->last = NULL;
+	list->first = NULL;
 }
 
-void	clean_list(int_list **list)
+void	push_front(edge *list, int number)
 {
-	int_list *tmp;
+	int_list	*new_element;
 
-	if (*list)
+	new_element = malloc(sizeof(int_list));
+	if (!new_element)
+		return ;
+	new_element->number = number;
+	new_element->prev = list->first;
+	new_element->next = NULL;
+	if (list->first)
+		list->first->next = new_element;
+	else
+		list->last = new_element;
+	list->first = new_element;
+}
+
+void	push_back(edge *list, int number)
+{
+	int_list	*new_element;
+
+	new_element = malloc(sizeof(int_list));
+	if (!new_element)
+		return ;
+	new_element->number = number;
+	new_element->next = list->last;
+	new_element->prev = NULL;
+	if (list->last)
+		list->last->prev = new_element;
+	else
+		list->first = new_element;
+	list->last = new_element;
+}
+
+void	clean_list(edge *list)
+{
+	int_list	*tmp;
+
+	while (list->last)
 	{
-		tmp = (*list)->previous;
-		free(*list);
-		*list = tmp;
+		tmp = list->first->prev;
+		free(list->first);
+		list->first = tmp;
 	}
+	init(list);
 }
