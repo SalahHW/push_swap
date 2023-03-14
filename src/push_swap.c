149e88088e3d369dc206c052036fa76ececf26e6
salah <salah.bouheniche@gmail.com>
2023-03-14 14:48:08 +0100

    update for testing

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,24 +6,19 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 20:25:02 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/03/11 00:58:51 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/03/14 07:51:38 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/push_swap.h"
+#include "../include/push_swap.h"
 
 int	main(int argc, char **argv)
 {
-	int_list *a_list;
-	int_list *b_list;
+	edge a_list;
+	edge b_list;
 
-	a_list = NULL;
-	b_list = NULL;
+	init(&a_list);
+	init(&b_list);
 	extract_values(&a_list, argc, argv);
-	push_b(&a_list, &b_list);
-	push_b(&a_list, &b_list);
-	push_a(&a_list, &b_list);
-	push_a(&a_list, &b_list);
-	push_a(&a_list, &b_list);
 	return (0);
 }
