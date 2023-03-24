796bfa5b6b09a4cf24b9f6c6276ffa07e0f2ec68
SalahHW <salah.bouheniche@gmail.com>
2023-03-24 01:26:15 +0100

    add push_front() push_back() pop_front() pop_back()

--- src/list_utils.c
+++ src/list_utils.c
@@ -6,74 +6,80 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 21:47:26 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/03/16 18:35:34 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/03/22 17:11:53 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-void	init(edge *list)
+void	init(t_edge *list)
 {
 	list->last = NULL;
 	list->first = NULL;
 }
 
-void	push_front(edge *list, int number)
+void  push_front(t_edge *list, int value)
 {
-	t_list	*new_element;
+  t_stack *new_element;
 
-	new_element = malloc(sizeof(t_list));
-	if (!new_element)
-		return ;
-	new_element->number = number;
-	new_element->prev = list->first;
-	new_element->next = NULL;
-	if (list->first)
-		list->first->next = new_element;
-	else
-		list->last = new_element;
-	list->first = new_element;
-}
-
-void	push_back(edge *list, int number)
-{
-	t_list	*new_element;
-
-	new_element = malloc(sizeof(t_list));
-	if (!new_element)
-		return ;
-	new_element->number = number;
-	new_element->next = list->last;
-	new_element->prev = NULL;
-	if (list->last)
-		list->last->prev = new_element;
-	else
-		list->first = new_element;
-	list->last = new_element;
-}
-
-void	pop_front(edge *list)
-{
-	t_list	*tmp;
-
-	if (!list->first)
+  new_element = malloc(sizeof(t_stack));
+  if (!new_element)
     return ;
-  tmp = list->first->next;
-  free(list->first);
-  list->first = tmp;
+  new_element->number = value;
+  new_element->next = list->first;
+  new_element->prev = NULL;
+  if (list->first)
+    list->first->prev = new_element;
+  else
+    list->last = new_element;
+  list->first = new_element;
 }
 
-void	clear_list(edge *list)
+void  push_back(t_edge *list, int value)
 {
-	t_list	*tmp;
-	t_list	*first_elem;
+  t_stack *new_element;
 
-	first_elem = list->first;
-	while (first_elem)
-	{
-		tmp = first_elem;
-		first_elem = first_elem->prev;
-		free(tmp);
-	}
-	init(list);
+  new_element = malloc(sizeof(t_stack));
+  if (!new_element)
+    return ;
+  new_element->number = value;
+  new_element->prev = list->last;
+  new_element->next = NULL;
+  if (list->last)
+    list->last->next = new_element;
+  else
+    list->first = new_element;
+  list->last = new_element;
+}
+
+void  pop_front(t_edge *list)
+{
+  t_stack *tmp;
+
+  if (list->first)
+  {
+    tmp = list->first;
+    list->first = list->first->next;
+    if (list->first)
+      list->first->prev = NULL;
+    else
+      list->last = NULL;
+    free(tmp);
+  }
+}
+
+void  pop_back(t_edge *list)
+{
+  t_stack *tmp;
+
+  if (list->last)
+  {
+    tmp = list->last;
+    list->last = list->last->prev;
+    if (list->last)
+      list->last->next = NULL;
+    else
+      list->first = NULL;
+    free(tmp);
+  }
 }
