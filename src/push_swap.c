c047ee3228ee64cfdde396e75151acc26f51d0ff
SalahHW <salah.bouheniche@gmail.com>
2023-03-24 03:12:56 +0100

    update for testing purpose

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 20:25:02 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/03/16 18:36:44 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/03/24 03:04:43 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,8 @@ int	main(int argc, char **argv)
 	init(&a_list);
 	init(&b_list);
 	extract_values(&a_list, argc, argv);
-  swap_a(&a_list);
   push_b(&a_list, &b_list);
   push_b(&a_list, &b_list);
-  swap_both(&a_list, &b_list);
+  reverse_rotate_both(&a_list, &b_list);
 	return (0);
 }
