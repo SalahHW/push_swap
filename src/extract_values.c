83ede710a6ccf4f0142447139bda65ded2a3c62b
SalahHW <salah.bouheniche@gmail.com>
2023-03-24 01:25:26 +0100

    use the new protoypes of functions

--- src/extract_values.c
+++ src/extract_values.c
@@ -6,14 +6,14 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 13:39:24 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/03/14 07:51:38 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/03/22 19:41:00 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
 #include "../include/push_swap.h"
 
-int	extract_values(edge *list, int argc, char **argv)
+int	extract_values(t_edge *list, int argc, char **argv)
 {
 	int	i;
 	int	n;
