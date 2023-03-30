cf56c8365d174502f6ee7fc9bb39720bf6e03109
Salah <salah.bouheniche@gmail.com>
2023-03-30 15:51:42 +0200

    update for testing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 15:13:50 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/03/30 15:28:36 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/03/30 15:40:32 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,9 @@ int	main(int argc, char **argv)
 		init(&a_list);
 		init(&b_list);
 		extract_values(&a_list, argc, argv);
+    push_b(&a_list, &b_list);
+    push_b(&a_list, &b_list);
+    push_b(&a_list, &b_list);
 	}
+  return (0);
 }
