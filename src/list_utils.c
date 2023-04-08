1ec7f73535cac7913c085999548f30c0eff6c01f
Salah <salah.bouheniche@gmail.com>
2023-04-08 10:15:24 +0200

    add a function to check if a list got dupplicated numbers

--- src/list_utils.c
+++ src/list_utils.c
@@ -6,40 +6,68 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 13:39:24 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/04/05 02:17:53 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/04/08 10:08:55 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
 #include "../include/push_swap.h"
 
-void  init(t_edge *list)
+void	init(t_edge *list)
 {
-  list->first = NULL;
-  list->last = NULL;
+	list->first = NULL;
+	list->last = NULL;
 }
 
-int check_values(t_stack *list)
+int	check_values(char **argv)
 {
-  while (list != NULL)
-    {
-      if (!is_integer(list->number))
-        return (0);
-      list = list->next; 
-    }
-  return (1);
+	argv++;
+	while (*argv)
+	{
+		if (!is_convertible_to_int(*argv))
+			return (0);
+		argv++;
+	}
+	return (1);
 }
 
 int	extract_values(t_edge *list, int argc, char **argv)
 {
-	int	i;
 	int	n;
 
-	i = argc;
-	while (--i > 0)
+	if (!check_values(argv))
+		return (0);
+	while (--argc > 0)
 	{
-		n = ft_atoi(argv[i]);
+		n = ft_atoi(argv[argc]);
 		push_front(list, n);
+	}
+	if (has_duplicate(list))
+	{
+		clear_list(list);
+		return (0);
+	}
+	return (1);
+}
+
+int	has_duplicate(t_edge *list)
+{
+	int		number;
+	t_stack	*list_first_elem_ptr;
+	t_stack	*list_ptr;
+
+	list_first_elem_ptr = list->first;
+	while (list_first_elem_ptr)
+	{
+		list_ptr = list_first_elem_ptr->next;
+		number = list_first_elem_ptr->number;
+		while (list_ptr)
+		{
+			if (number == list_ptr->number)
+				return (1);
+			list_ptr = list_ptr->next;
+		}
+		list_first_elem_ptr = list_first_elem_ptr->next;
 	}
 	return (0);
 }
