619241f3f801373c9ed73457c5517b4686864aa8
Salah <salah.bouheniche@gmail.com>
2023-04-08 10:17:20 +0200

    add new prototypes

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 20:26:52 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/04/05 01:54:49 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/04/08 09:47:39 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,15 @@ typedef struct
 
 ///       list_utils.c        ///
 void				init(t_edge *list);
+int					check_values(char **argv);
 int					extract_values(t_edge *listname, int argc, char **argv);
+int					has_duplicate(t_edge *list);
 ///			list_movements.c		///
 void				push_front(t_edge *list, int value);
 void				push_back(t_edge *list, int value);
 void				pop_front(t_edge *list);
 void				pop_back(t_edge *list);
+void				clear_list(t_edge *list);
 ///			swap.c				///
 void				swap_a(t_edge *a_list);
 void				swap_b(t_edge *b_list);
@@ -52,5 +55,8 @@ void				rotate_both(t_edge *a_list, t_edge *b_list);
 void				reverse_rotate_a(t_edge *a_list);
 void				reverse_rotate_b(t_edge *b_list);
 void				reverse_rotate_both(t_edge *a_list, t_edge *b_list);
+///     int_convert_utils.c   ///
+int					is_convertible_to_int(char *str);
+long				ft_atol(char *str);
 
 #endif
