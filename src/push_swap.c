e5d5206c2b7ff8108f4dacefea3c8c9ab41eaa65
Salah <salah.bouheniche@gmail.com>
2023-05-12 16:46:02 +0200

    add functions

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,12 +6,15 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 20:25:02 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/04/09 11:02:36 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/05/12 16:37:34 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/libft.h"
 #include "../include/push_swap.h"
 
-void	push_swap(int argc, char *argv[])
+void	push_swap(t_edge *a_list, t_edge *b_list)
 {
+	if (!is_sorted(a_list))
+		sort(a_list, b_list);
 }
