73ee1dcf651b3f04ee9df43d84a152d3f7ac0383
Salah <salah.bouheniche@gmail.com>
2023-05-20 09:33:37 +0200

    fix several bugs and add new functions

--- src/position.c
+++ src/position.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 17:34:06 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/05/17 06:03:59 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/05/19 19:59:14 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,44 @@
 
 int	find_position(t_edge *list, int num)
 {
-	int		i;
+	int		position;
 	t_stack	*element;
 
 	element = list->first;
-	i = 0;
+	position = 0;
 	while (element->number != num)
 	{
 		element = element->next;
-		i++;
+		position++;
 	}
-	return (i);
+	return (position);
 }
 
-int	find_futur_position(t_stack *element, t_edge *b_list)
+int	find_futur_position_in_a(t_stack *element, t_edge *a_list)
+{
+	t_stack	*a_list_ptr;
+	int		number_b;
+	int		position;
+
+	number_b = element->number;
+	position = 0;
+	if (number_b < a_list->first->number && number_b > a_list->last->number)
+		return (0);
+	else if (number_b > greatest_number(a_list)
+			|| number_b < smallest_number(a_list))
+		return (find_position(a_list, smallest_number(a_list)));
+	else
+	{
+		a_list_ptr = a_list->first;
+		while (a_list_ptr->number > number_b
+			|| a_list_ptr->next->number < number_b)
+			a_list_ptr = a_list_ptr->next;
+		position = find_position(a_list, a_list_ptr->next->number);
+	}
+	return (position);
+}
+
+int	find_futur_position_in_b(t_stack *element, t_edge *b_list)
 {
 	t_stack	*b_list_ptr;
 	int		number_a;
@@ -58,4 +82,33 @@ int	find_futur_position(t_stack *element, t_edge *b_list)
 			return (find_position(b_list, next_number_b));
 	}
 	return (0);
+}
+
+void	final_rotation(t_edge *a_list, t_edge *b_list)
+{
+	t_stack	*b_element;
+	int		futur_position;
+
+	while (lst_length(b_list) > 0)
+	{
+		b_element = b_list->first;
+		futur_position = find_futur_position_in_a(b_element, a_list);
+		while (futur_position != 0)
+		{
+			if (futur_position > lst_length(a_list) / 2 + 1)
+				reverse_rotate_a(a_list);
+			else
+				rotate_a(a_list);
+			futur_position = find_futur_position_in_a(b_element, a_list);
+		}
+		push_a(a_list, b_list);
+	}
+	while (a_list->first->number != smallest_number(a_list))
+	{
+		if (find_position(a_list, smallest_number(a_list)) > lst_length(a_list)
+			/ 2 + 1)
+			reverse_rotate_a(a_list);
+		else
+			rotate_a(a_list);
+	}
 }
--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 02:41:20 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/05/17 05:56:48 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/05/19 20:06:53 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,36 +36,45 @@ static void	sort_three_element(t_edge *a_list)
 
 static void	deep_sort(t_edge *a_list, t_edge *b_list)
 {
-	t_stack	*a_element;
+	t_move	*a_moves;
 
 	while (lst_length(a_list) > 3)
 	{
 		reset_cost(a_list);
 		calculate_move(a_list, b_list);
-		a_element = find_cheapest_move(a_list);
-		execute_cheapest_move(a_list, b_list, a_element);
+		a_moves = find_cheapest_move(a_list);
+		execute_cheapest_move(a_list, b_list, a_moves);
 	}
-	sort_three_element(a_list);
-	while (b_list->first)
-		push_a(a_list, b_list);
+	if (!is_sorted(a_list))
+		sort_three_element(a_list);
+	final_rotation(a_list, b_list);
 }
 
 void	execute_cheapest_move(t_edge *a_list, t_edge *b_list,
-		t_stack *a_element)
+		t_move *a_moves)
 {
-	while (a_element->ra-- > 0 && a_element->rb-- > 0)
+	while (a_moves->ra > 0 && a_moves->rb > 0)
+	{
+		a_moves->ra--;
+		a_moves->rb--;
 		rotate_both(a_list, b_list);
-	while (a_element->rra-- > 0 && a_element->rrb-- > 0)
+	}
+	while (a_moves->rra > 0 && a_moves->rrb > 0)
+	{
+		a_moves->rra--;
+		a_moves->rrb--;
 		reverse_rotate_both(a_list, b_list);
-	while (a_element->ra-- > 0)
+	}
+	while (a_moves->ra-- > 0)
 		rotate_a(a_list);
-	while (a_element->rra-- > 0)
+	while (a_moves->rra-- > 0)
 		reverse_rotate_a(a_list);
-	while (a_element->rb-- > 0)
+	while (a_moves->rb-- > 0)
 		rotate_b(b_list);
-	while (a_element->rrb-- > 0)
+	while (a_moves->rrb-- > 0)
 		reverse_rotate_b(b_list);
 	push_b(a_list, b_list);
+	free(a_moves);
 }
 
 void	sort(t_edge *a_list, t_edge *b_list)
@@ -73,13 +82,14 @@ void	sort(t_edge *a_list, t_edge *b_list)
 	if (lst_length(a_list) < 3)
 		rotate_a(a_list);
 	else if (lst_length(a_list) == 3)
-		sort_three_element(a_list);
+	{
+		if (!is_sorted(a_list))
+			sort_three_element(a_list);
+	}
 	else
 	{
 		push_b(a_list, b_list);
 		push_b(a_list, b_list);
-		if (!is_reverse_sorted(b_list))
-			rotate_b(b_list);
 		deep_sort(a_list, b_list);
 	}
 }
