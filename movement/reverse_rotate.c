719b85c7e49fc164d97b0367a37bd018bbb91949
Salah <salah.bouheniche@gmail.com>
2023-05-20 09:27:57 +0200

    no modification

--- movement/reverse_rotate.c
+++ movement/reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 02:54:37 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/03/30 15:29:05 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/05/17 17:08:09 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ void	reverse_rotate_b(t_edge *b_list)
 void	reverse_rotate_both(t_edge *a_list, t_edge *b_list)
 {
 	push_front(a_list, a_list->last->number);
-	push_front(b_list, b_list->last->number);
 	pop_back(a_list);
+	push_front(b_list, b_list->last->number);
 	pop_back(b_list);
 	write(1, &"rrr\n", 4);
 }
