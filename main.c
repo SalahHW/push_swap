36e93481a2ac1e5cf6ee5445f974eb30781ec8ad
Salah <salah.bouheniche@gmail.com>
2023-05-20 09:26:48 +0200

    test new sorting

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 15:13:50 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/05/16 23:09:50 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/05/19 17:03:27 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,9 @@ int	main(int argc, char **argv)
 	init(&b_list);
 	if (!extract_values(&a_list, argc, argv))
 		return (print_error());
-	reset_cost(&a_list);
 	if (!is_sorted(&a_list))
 		sort(&a_list, &b_list);
+	clear_list(&a_list);
+	clear_list(&b_list);
 	return (0);
 }
