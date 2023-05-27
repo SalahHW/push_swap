db746eb3c562823452056589a52759ce920120e8
Salah <salah.bouheniche@gmail.com>
2023-05-28 01:51:17 +0200

    update for testing

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,12 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 15:13:50 by sbouheni          #+#    #+#             */
-/*   Updated: 2023/05/27 03:01:20 by sbouheni         ###   ########.fr       */
+/*   Updated: 2023/05/27 22:32:18 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/push_swap.h"
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
@@ -23,11 +24,6 @@ int	main(int argc, char **argv)
 	init(&b_list);
 	if (!extract_values(&a_list, argv))
 		return (exit_error());
-	if (lst_length(&a_list) < 2)
-	{
-		clear_list(&a_list);
-		return (0);
-	}
 	if (!is_sorted(&a_list))
 		sort(&a_list, &b_list);
 	clear_list(&a_list);
