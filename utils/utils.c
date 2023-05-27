8e2c3473b3bd1862857cf47887a058796f4a4041
Salah <salah.bouheniche@gmail.com>
2023-05-27 03:21:44 +0200

    add new file to add functions

---
+++ utils/utils.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 16:46:37 by sbouheni          #+#    #+#             */
+/*   Updated: 2023/05/27 02:56:31 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+void	init(t_edge *list)
+{
+	list->first = NULL;
+	list->last = NULL;
+}
+
+int	exit_error(void)
+{
+	ft_putstr_fd("Error\n", 1);
+	exit(1);
+}
